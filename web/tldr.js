269907e361d31303bc99ce73a5df6757fcab52e3
gameinbucket <nathannoob@gmail.com>
2015-10-23 00:45:36 -0700

    Update tldr.js

--- web/tldr.js
+++ web/tldr.js
@@ -170,37 +170,6 @@ function analyze(input)
 
     out = chem.solve(input);
     if (out) return out;
-
-    // context menu
-    // copy title
-    // copy content
-    // save
-    // close
-
-    // calculated
-    // time [location] [location]
-    // analog clock time
-    // digital clock time
-    // time zone [location]
-    // measurements [value] [type] [value] [type]
-    // arithmetic
-    // algebra
-    // calculus
-    // atomic mass [molecule]
-    // chem[istry] equation balance
-    // b[ase] 2 (binary) , base 8 (octal), base 10 (decimal), base 16 (hex), base 64 conversion
-    // boolean algebra simplify / truth table 
-    
-    // database
-    // news [date]
-    // english grammar words
-    // chemistry equations
-    // physics equations
-    // list of acids and bases [molecule]
-    // weather [location] [day]
-    // history
-    // music player <audio>
-    // cooking times and instructions
     
     return null;
 }
